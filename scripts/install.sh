80054f5672cc2b45fd1da72667324532305565bc
nok911 <niallocaoimh3@gmail.com>
2023-01-24 21:39:02 +0000

    add-scripts

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl jq
+
+#installs docker
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+
+
+#installs docker compose
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+#aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
